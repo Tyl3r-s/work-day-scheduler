4b175a3a42687ffb8268f2cf81daeeaf18d3937a
Tyler <Tyl3r.s@outlook.com>
2022-06-08 18:13:44 -0400

    fixed colouring and got local storage working

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,4 @@
-var tasks = {};
+
 
 var clock = document.getElementById("currentTime");
 
@@ -18,6 +18,8 @@ setInterval(todaysDate, 1000);
 
 setInterval(clockTimer, 1000);
 
+setInterval(hourTracker, 1000); 
+
 function hourTracker() {
 
 $(".taskText").each(function(i) {
@@ -34,43 +36,8 @@ $(".taskText").each(function(i) {
 })
 };
 
-var loadTasks = function() {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-    if (!tasks) {
-      tasks = {
-        toDo: []
-      };
-    }
-  
-    // loop over object properties
-    $.each(tasks, function(list, arr) {
-      console.log(list, arr);
-      // then loop over sub-array
-      arr.forEach(function(task) {
-        createTask(task.text, task.date, list);
-      });
-    });
-  };
-
-var saveTasks = function () {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-};
-
-// $(".list-group").on("click", "p", function () {
-//     // get current text of p element
-//     var text = $(this)
-//         .text()
-//         .trim();
-//     var textInput = $("<textarea>").addClass("form-control").val(text);
-//     $(this).replaceWith(textInput);
-//     textInput.trigger("focus");
-// });
-
 $(".list-group").on("blur", "textarea", function () {
-    // get current value of textarea
     var text = $(this).val();
-
-    // get status type and position in the list
     var status = $(this)
         .closest(".list-group")
         .attr("id")
@@ -78,19 +45,17 @@ $(".list-group").on("blur", "textarea", function () {
     var index = $(this)
         .closest(".list-group-item")
         .index();
-
-    // update task in array and re-save to localstorage
     tasks[status][index].text = text;
     saveTasks();
-
-    // recreate p element
     var taskP = $("<p>")
         .addClass("m-1")
         .text(text);
-
-    // replace textarea with new content
     $(this).replaceWith(taskP);
 });
 
-hourTracker();
+$(".timeBlock").on("click", ".saveBtn", function () {
+    var tasktext = $(this).parent().find(".taskText").val().trim();
+    var time = $(this).parent().find(".taskText").attr("id");
+    localStorage.setItem(time, tasktext);
+  });
 
