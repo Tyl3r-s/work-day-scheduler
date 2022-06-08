8559671b82c30c7049bc63dcca94fb0f3573a6cd
Tyler <Tyl3r.s@outlook.com>
2022-06-08 18:48:11 -0400

    local storage working

--- assets/script/script.js
+++ assets/script/script.js
@@ -20,6 +20,29 @@ setInterval(clockTimer, 1000);
 
 setInterval(hourTracker, 1000); 
 
+function loadTasks() {
+    var text9 = localStorage.getItem(9);
+    var text10 = localStorage.getItem(10);
+    var text11 = localStorage.getItem(11);
+    var text12 = localStorage.getItem(12);
+    var text13 = localStorage.getItem(13);
+    var text14 = localStorage.getItem(14);
+    var text15 = localStorage.getItem(15);
+    var text16 = localStorage.getItem(16);
+    var text17 = localStorage.getItem(17);
+    $("#9").val(text9);
+    $("#10").val(text10);
+    $("#11").val(text11);
+    $("#12").val(text12);
+    $("#13").val(text13);
+    $("#14").val(text14);
+    $("#15").val(text15);
+    $("#16").val(text16);
+    $("#17").val(text17);
+}
+
+loadTasks();
+
 function hourTracker() {
 
 $(".taskText").each(function(i) {
@@ -46,15 +69,16 @@ $(".list-group").on("blur", "textarea", function () {
         .closest(".list-group-item")
         .index();
     tasks[status][index].text = text;
-    saveTasks();
     var taskP = $("<p>")
         .addClass("m-1")
         .text(text);
     $(this).replaceWith(taskP);
 });
 
-$(".timeBlock").on("click", ".saveBtn", function () {
-    var tasktext = $(this).parent().find(".taskText").val().trim();
+$(".saveBtn").on("click", function () {
+    var tasktext = $(this).parent().find(".taskText")
+    .val()
+    .trim();
     var time = $(this).parent().find(".taskText").attr("id");
     localStorage.setItem(time, tasktext);
   });
