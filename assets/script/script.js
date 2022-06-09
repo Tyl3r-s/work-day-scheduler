d400b2ebda374268763354d48ed17c148c335110
Tyler <Tyl3r.s@outlook.com>
2022-06-08 23:44:12 -0400

    added comments up in there + final touches

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,4 @@
-
+// this top section holds all the scripts pertaining to date and time
 
 var clock = document.getElementById("currentTime");
 
@@ -6,20 +6,23 @@ var date = document.getElementById("todaysDate")
 
 const currentHour = moment().format("H");
 
+// grabs moment.js time data and full sends it to the clock variable
 function clockTimer() {
     clock.textContent = moment().format("LTS");
 }
-
+// same as clock but date
 function todaysDate() {
     date.textContent = moment().format("dddd MMMM Do YYYY");
 }
 
+// these intervals make the whole page go nuts just running these functions every single second
 setInterval(todaysDate, 1000);
 
 setInterval(clockTimer, 1000);
 
 setInterval(hourTracker, 1000); 
 
+// this script will check local storage and update the task boxes as per their number
 function loadTasks() {
     var text9 = localStorage.getItem(9);
     var text10 = localStorage.getItem(10);
@@ -41,8 +44,10 @@ function loadTasks() {
     $("#17").val(text17);
 }
 
+//fire the lazers (and function)
 loadTasks();
 
+// this part keeps track of which task box shoud be what colour (class)
 function hourTracker() {
 
 $(".taskText").each(function(i) {
@@ -59,6 +64,8 @@ $(".taskText").each(function(i) {
 })
 };
 
+//I'm not sure what this does. It's magic. I'm just kidding, it does.. something
+//(it brings up the edit box)
 $(".list-group").on("blur", "textarea", function () {
     var text = $(this).val();
     var status = $(this)
@@ -75,6 +82,7 @@ $(".list-group").on("blur", "textarea", function () {
     $(this).replaceWith(taskP);
 });
 
+// this save button code commits the task box stuff to local storage via event listener on the save button
 $(".saveBtn").on("click", function () {
     var tasktext = $(this).parent().find(".taskText")
     .val()
